5e768db8d5feb6036bc1ca38fe026167448a1492
rogeliorv <rogeliorv@gmail.com>
2024-07-17 15:00:21 +0300

    chore: opened image api to the public

--- apps/web/app/api/generateImage/route.ts
+++ apps/web/app/api/generateImage/route.ts
@@ -1,28 +1,57 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { kv } from "@vercel/kv";
 import OpenAI from "openai";
+import { NextRequest, NextResponse } from "next/server";
+import type { NextApiRequest } from "next";
 
-export const runtime = "edge";
+const allowedOrigins = ['http://localhost:5000', 'https://yourapp.vercel.app'];
 
-export async function POST(req: Request): Promise<Response> {
+export async function OPTIONS(req: NextApiRequest, res: NextResponse): Promise<NextResponse> {
+
+  const origin = req.headers.origin;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    return new NextResponse(null, {
+      headers: {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+        'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+      },
+      status: 200,
+    });
+  }
+
+  return new NextResponse(null, {
+    headers: {
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    },
+    status: 200,
+  });
+}
+
+export async function POST(req: NextRequest, res: NextResponse): Promise<NextResponse> {
   const { prompt, apiKey } = await req.json();
 
   const openAIApiKey = apiKey || process.env.OPENAI_API_KEY;
 
   if(new Date() > new Date('2024-08-20')) {
-    return new Response(`This API has expired`, {
+    return new NextResponse(`This API has expired`, {
       status: 410,
     });
   }
 
   // Check if the OPENAI_API_KEY is set, if not return 400
   if (!openAIApiKey || openAIApiKey === "") {
-    return new Response(`Missing OPENAI_API_KEY - make sure to configure it. You used: ${openAIApiKey}`, {
+    return new NextResponse(`Missing OPENAI_API_KEY - make sure to configure it. You used: ${openAIApiKey}`, {
       status: 400,
     });
   }
+
   if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
-    const ip = req.headers.get("x-forwarded-for");
+    const ip = req.headers["x-forwarded-for"];
     const ratelimit = new Ratelimit({
       redis: kv,
       limiter: Ratelimit.slidingWindow(50, "1 d"),
@@ -31,7 +60,7 @@ export async function POST(req: Request): Promise<Response> {
     const { success, limit, reset, remaining } = await ratelimit.limit(`lotion_ratelimit_${ip}`);
 
     if (!success) {
-      return new Response("You have reached your request limit for the day.", {
+      return new NextResponse("You have reached your request limit for the day.", {
         status: 429,
         headers: {
           "X-RateLimit-Limit": limit.toString(),
@@ -56,10 +85,10 @@ export async function POST(req: Request): Promise<Response> {
     });
 
     const imgUrl = response.data[0].url || '';
-    return new Response(JSON.stringify({url: imgUrl}));
+    return new NextResponse(JSON.stringify({url: imgUrl}));
 
   } catch(error) {
-    return new Response(error.message, {
+    return new NextResponse(error.message, {
       status: 400,
     });
   }
